5726bee9f687d1a355a10e5a48a3b929652eb192
MariolyTintaya <tintayaramosmarioly@gmail.com>
2024-11-22 19:55:54 -0400

    intento de hacer plantilla

---
+++ resources/views/components/panel.blade.php
@@ -0,0 +1,8 @@
+
+<!-- resources/views/reservas/create.blade.php -->
+@extends('layouts.panel')
+
+@section('content')
+    <h2>Crear Reserva</h2>
+    <!-- Contenido específico para la creación de una reserva -->
+@endsection
